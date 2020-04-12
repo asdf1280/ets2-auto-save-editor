5334abfc8647ed4eb6fb620aa053485a6dedddbf
User <dhkim0800@noreply.github.com>
2020-04-12 12:14:57 +0900

    Change version to 1.03 Alpha

--- ETS2SaveAutoEditor/MainWindow.xaml.cs
+++ ETS2SaveAutoEditor/MainWindow.xaml.cs
@@ -52,7 +52,7 @@ namespace ETS2SaveAutoEditor
     /// </summary>
     public partial class MainWindow : Window
     {
-        public static string Version = "1.02 Alpha";
+        public static string Version = "1.03 Alpha";
         public static byte[] StringToByteArray(String hex)
         {
             int NumberChars = hex.Length / 2;

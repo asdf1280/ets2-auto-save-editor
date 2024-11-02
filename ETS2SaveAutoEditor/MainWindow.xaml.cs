96f1a199ef27da4e8dd4d0df2678cc50b48d77cb
D <10652364+asdf1280@users.noreply.github.com>
2024-11-02 16:12:17 +0900

    version

--- ETS2SaveAutoEditor/MainWindow.xaml.cs
+++ ETS2SaveAutoEditor/MainWindow.xaml.cs
@@ -66,7 +66,7 @@ namespace ETS2SaveAutoEditor {
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class MainWindow : Window {
-        public static string Version = "1.30";
+        public static string Version = "1.30.1";
 
         private SaveeditTasks tasks = new();
 
